ba9c94128db2451eb86bdb3268d22acd25c15c5e
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-15 09:35:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+let empty_array = []
+  for (const driver of drivers) {
+    if (driver.toUpperCase() === name.toUpperCase()) {
+      empty_array.push(driver)
+    }
+  }
+  return empty_array
+}
+
+function fuzzyMatch(drivers, first_letters) {
+  let empty_array = []
+  for (const driver of drivers) {
+    if (driver.slice(0,2) === first_letters) {
+      empty_array.push(driver)
+    }
+  }
+  return empty_array
+}
+
+function matchName(drivers, driver_name) {
+  let empty_array = []
+  for (const driver of drivers) {
+    if (driver.name === driver_name) {
+      empty_array.push(driver)
+    }
+  }
+  return empty_array
+}
