3cdeaa710894b60be3a12855893665098ce6e133
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-06-19 10:02:48 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch(drivers, partial){
+  let beginningName = partial.length;
+  return drivers.filter(function (driverName){
+    return driverName.slice(0, beginningName) === partial
+  })
+
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
